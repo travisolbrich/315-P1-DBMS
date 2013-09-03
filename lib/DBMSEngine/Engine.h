ddf7bd2083491295175942b08dcc14317ded603b
Shane Buckner <shaneb_333@hotmail.com>
2013-09-03 12:38:26 -0500

    Added Engine.h
    It has been a while since I wrote classes in C++, so please pardon any
    and all errors and downfalls at this beginning.

---
+++ lib/DBMSEngine/Engine.h
@@ -0,0 +1,27 @@
+//Engine Header
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <array>
+
+class Relations {
+
+	Create_Table {};
+
+	open_ {};
+
+	close_ {};
+
+	write_ {};
+
+	exit_ {};
+
+	show_ {};
+
+	update_ {};
+
+	insert_ {};
+
+	delete_ {};
+}
