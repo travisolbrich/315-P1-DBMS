558ce0db47b54eb8ef48dc0bfaf3d12e4f653525
Shane Buckner <shaneb_333@hotmail.com>
2013-09-03 01:32:19 -0500

    Added DB Engine folder and DB_Engine.cpp
    Engine cpp is the pseudocode we came up with previously. Decent starting
    template.

---
+++ lib/DBMS Engine/DB_Engine.cpp
@@ -0,0 +1,30 @@
+/** Start for DB Engine
+  * Pseudocode previously used for now
+  */
+class table
+	-> array of attributes
+	-> vector of tuple
+		-> tuple is array of values
+
+/////////////////////////////////////////////////////////////////////////
+
+table ||    people
+id    ||   f_name   ||   l_name
+-------------------------------------------------------------------------
+0     ||   travis   ||   olbrich
+1     ||   john     ||   leslie
+-------------------------------------------------------------------------
+		
+/////////////////////////////////////////////////////////////////////////
+
+class table
+	table name = people
+	array attributes = {
+		[int id]
+		[str f_name]
+		[str l_name] 
+	}
+	vector tuple = {
+		array {0, "travis", "olbrich"}
+		array {1, "john", "leslie"}
+	}
