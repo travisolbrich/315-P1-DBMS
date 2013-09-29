aded5d73cf2a2bcc794a43f10b1787cd6d362608
adampitones <pitonesadam@gmail.com>
2013-09-29 13:48:34 -0500

    Changed some syntax errors so it could compile

--- lib/DBApplication/DBAppMenu.cpp
+++ lib/DBApplication/DBAppMenu.cpp
@@ -1,9 +1,10 @@
-#include "DBAppMenu.h"
+//#include "DBAppMenu.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 int menu = 0;
@@ -13,39 +14,40 @@ int main(int argc, char const *argv[])
 {
 	while(true)
 	{
-	cout << "<<<<< Main Menu >>>>> /n/n/n";
-	cout << "Menu Options: /n
-			1) Location table /n
-			2) Sale table /n
-			3) Employee table /n
-			4) Role table /n
-			5) Report functions /n
-			99) EXIT /n";
+	cout << "<<<<< Main Menu >>>>> \n\n\n";
+	cout << "Menu Options: \n"
+			<<"1) Location table \n"
+			<<"2) Sale table \n"
+			<<"3) Employee table \n"
+			<<"4) Role table \n"
+			<<"5) Report functions \n"
+			<<"9) Back / Previous Option \n"
+			<<"99) EXIT \n";
 	cin >> menu;
 		switch (menu)
 		{
 			case 0:		//main menu
 				//link: 1,2,3,4,5, 9(back)
-				cout << "<<<<< Main Menu >>>>> /n/n/n";
-				cout << "Menu Options: /n
-						1) Location table /n
-						2) Sale table /n
-						3) Employee table /n
-						4) Role table /n
-						5) Report functions /n
-						9) Back / Previous Option /n
-						99) EXIT /n";
+				cout << "<<<<< Main Menu >>>>> \n\n\n";
+				cout << "Menu Options: \n"
+						<<"1) Location table \n"
+						<<"2) Sale table \n"
+						<<"3) Employee table \n"
+						<<"4) Role table \n"
+						<<"5) Report functions \n"
+						<<"9) Back / Previous Option \n"
+						<<"99) EXIT \n";
 				cin >> menu;
 				break;
 
 			case 1:		//location
 				//Link: 0,2,3,11, 9(back)
-				cout << "<<<<< Location Sub-Menu >>>>> /n/n/n";
-				cout << "Menu Options: /n
-						0) Return to Main Menu /n
-						1) Sale Table /n
-						2) Employee Table /n
-						11) Location View /n";
+				cout << "<<<<< Location Sub-Menu >>>>> \n\n\n";
+				cout << "Menu Options: \n"
+						<<"0) Return to Main Menu \n"
+						<<"1) Sale Table \n"
+						<<"2) Employee Table \n"
+						<<"11) Location View \n";
 				cin >> submenu;
 				break;
 				switch(submenu)
@@ -58,8 +60,8 @@ int main(int argc, char const *argv[])
 					case 3: //emp
 
 					case 11: //locView
+					break;
 				}
-
 			case 2:		//sale
 				//Link: 0,1,3,21,22,23, 9(back)
 				switch(submenu)
@@ -75,6 +77,7 @@ int main(int argc, char const *argv[])
 					case 22: //saleAdd
 
 					case 23: //saleEdit
+					break;
 				}
 
 			case 3:		//employee
@@ -88,6 +91,7 @@ int main(int argc, char const *argv[])
 					case 2: //sale
 
 					case 31: //empView
+					break;
 				}
 
 			case 4:		//role
@@ -97,6 +101,7 @@ int main(int argc, char const *argv[])
 					case 0:	//main
 
 					case 41: //roleView
+					break;
 				}
 
 			case 5:		//report
@@ -124,7 +129,7 @@ int main(int argc, char const *argv[])
 
 			case 41: 	//viewRole
 				//view: open, close, exit, show, project, back
-			case 99;
+			case 99:
 				return false;
 				break;
 		}
